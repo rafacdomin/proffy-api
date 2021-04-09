0766a7dd91bb98e3ccebd9f34bd0e2cdec12482b
Rafael Domingues <40310160+rafacdomin@users.noreply.github.com>
2021-04-09 10:26:32 -0300

    Update connection.ts

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -8,6 +8,7 @@ const db = knex({
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
+    ssl: true,
   },
   useNullAsDefault: true,
 });
